1e52a24c0054f0768db359d4a8d3c22502f8aa79
GabrielTresch <gabriel_tresch@outlook.com>
2020-01-04 11:48:13 +0100

    dropbox main folder

--- src/containers/IconContainer.js
+++ src/containers/IconContainer.js
@@ -1,7 +1,29 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useEffect, useState } from 'react';
+import { Dropbox } from 'dropbox';
+import axiox from 'axios';
 
-const IconContainer = () => (
-  <h1>icons</h1>
-);
+const IconContainer = () => {
+  const accessToken = process.env.REACT_APP_TOKEN;
+  const dbx = new Dropbox({ accessToken, axiox });
+
+  const [folders, setFolders] = useState([]);
+
+  useEffect(() => {
+    dbx.filesListFolder({ path: '' }).then((data) => { setFolders(data.entries); });
+  }, []);
+
+  console.log(folders);
+  return (
+    <>
+      <h1>My dropbox icon library</h1>
+      <div>
+        {folders.map((value) => (
+          <p key={value.name}>{value.path_display}</p>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default IconContainer;
